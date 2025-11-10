8d055fcb10489b8718e8cfb4904cc1fb5b6a82f0
botszhuang <22925483+botszhuang@users.noreply.github.com>
2025-11-10 22:27:53 +0800

    javascript hw#1

--- Javascript_homework1/tool.js
+++ Javascript_homework1/tool.js
@@ -14,3 +14,35 @@ function isPrime(x) {
     }
         return true ;
 }
+
+function getPrime100( prime ){
+        const MAX = 100 ;
+        prime.push ( 2 ) ;
+        for ( let i = 3 ; i < MAX ; i++ ) {
+          if ( isPrime( i ) === true ) { prime.push( i ) ; }
+        }
+}
+
+function appendCells ( cellArray , prime , start , end, mainDiv){
+    
+    for ( let i = start ; i < end ; i++ ) {    
+        cellArray[ i ] = document.createElement ( 'div' ) ;
+        cellArray[ i ].classList.add ( "cellDiv" ) ;
+        cellArray[ i ].textContent = prime [ i ] ;
+        mainDiv.appendChild ( cellArray [ i ] ) ;
+    }
+
+}
+
+function cal_batchSize( mainDiv , cellArray ){
+
+        pp ( "main = " + mainDiv.clientHeight + ", " + mainDiv.clientWidth ) ;
+        pp ( "cell = " + cellArray.clientHeight + ", " + cellArray.clientWidth ) ;
+        
+        wSize = Math.floor ( mainDiv.clientWidth / cellArray.clientWidth ) ;
+        hSize = Math.floor( mainDiv.clientHeight / cellArray.clientHeight ) ;
+        batchSize = wSize * hSize ;
+        pp ( "wSize = " + wSize + ", hSize = " + hSize ) ;
+
+        return batchSize ;
+}
