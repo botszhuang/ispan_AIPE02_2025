c425907d7afa18894114ce1e8b9b1a8e135652f7
botszhuang <22925483+botszhuang@users.noreply.github.com>
2025-11-10 19:48:01 +0800

    func:isPrime, OK

--- Javascript_homework1/tool.js
+++ Javascript_homework1/tool.js
@@ -2,3 +2,15 @@ function pp ( x ) {
     //alert("Hello World!");
     console.log (x) ; 
 }
+
+function isPrime(x) {
+    //if (x   < 2) { return false; }
+    //if (x === 2) { return true;  }
+    if ( ( x % 2 ) == 0 ) { return false ; } 
+           
+    let iMax = Math.sqrt ( x ) +1 ;
+    for ( let i = 3 ; i < iMax ; i+=2 ) {
+        if ( (x % i) === 0 )  { return false ; }
+    }
+        return true ;
+}
