6ebc6a7d30c8df073ca04fae03d9ee356fe96993
botsz <22925483+botszhuang@users.noreply.github.com>
2025-11-14 09:14:15 +0800

    Revert "pull"
    This reverts commit 510bf9faee3967a882c6fb4d89357ec3ea9c5e27.

--- Javascript_gis_Ubike_Taipei/url2file.c
+++ Javascript_gis_Ubike_Taipei/url2file.c
@@ -1,13 +1,3 @@
-/*****************************************************
-1) emcc url2file.c cJSON.c -o hello.js  -s EXPORTED_FUNCTIONS='["_main"]'   -s EXPORTED_RUNTIME_METHODS='["cwrap"]'
-2) $ python3 -m http.server 8000
-   Then open: http://localhost:8000/index.html
-3) NOTE:
-Don’t <script src="hello.wasm"></script> → it’s binary, not JS.
-Include hello.js only.
-Access via http://localhost or another local server.
-******************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
