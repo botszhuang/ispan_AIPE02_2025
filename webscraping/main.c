345e2f94f88a9561ca2aeaf3565e94b81aa005f5
botszhuang <22925483+botszhuang@users.noreply.github.com>
2025-11-15 22:15:33 +0800

    lxml works, but func:webscraping is still under construction.

--- webscraping/main.c
+++ webscraping/main.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <curl/curl.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/xpath.h>
 #include <string.h>
 #include <locale.h>
-//#include <cjson/cJSON.h>
-#include "cJSON.h"
-// gcc cJSON.c url2file.c -o ur2file.o  -lcurl
 
+// gcc main.c -o main.o  -lcurl
 
-static size_t Write_to_a_string ( void * context , size_t size, size_t numb, void *stream ){
-    size_t newLength = size * numb ;
-    memcpy (  stream , context  ) ;
-    return newLength ;
+typedef struct {
+   char * data ;
+   unsigned int size ;
+}streamStruct;
+
+static size_t Write_to_a_string ( void * context , size_t size, size_t numb, void *stream ){ 
+    size_t Length = size * numb ; 
+    streamStruct * a = stream ;
+
+    char * ptr = realloc ( a->data , a->size + Length + 1 ) ;
+    if ( !ptr ) { return 0 ; }
+
+    a->data = ptr ;
+    memcpy ( a->data + ( a->size ) , context , Length ) ;
+    a->size += Length ;
+    a->data [ a->size ] = 0 ;
+    return Length ;
 }
-int donwloadFileFromURL(const char *fName, char *urlAddress)
-{
-    // const char *fName = "Ubike.json";
-    // char *urlAddress = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json";
+int curl_get(  streamStruct * buffer , char *urlAddress ){
 
-    CURLcode res;
-    CURL *curl;
-
-    if (fName == NULL || urlAddress == NULL){
+    if ( !urlAddress ){
         fprintf( stderr, "NO <URL>\n");
         return EXIT_FAILURE ;
     }
 
     /* init the curl session */
-    res = curl_global_init(CURL_GLOBAL_ALL);
+    CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
     if (res){   
         fprintf(stderr, "Could not init curl\n");   
         return (int)res;
     }
- 
-    curl = curl_easy_init();
+  
+    CURL * curl = curl_easy_init();
     if (!curl){
         curl_global_cleanup();
         return EXIT_FAILURE;
@@ -44,46 +50,78 @@ int donwloadFileFromURL(const char *fName, char *urlAddress)
     curl_easy_setopt(curl, CURLOPT_URL, urlAddress);
 
     /* Switch on full protocol/debug output while testing */
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-    /* disable progress meter, set to 0L to enable it */
-    // curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
     /* enable progress meter, set to 0L to enable it */
     curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
 
     /* send all data to this function  */
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-
-    /* write the page body to this file handle */
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-
-    /* get it! */
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Write_to_a_string );
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer );
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
     res = curl_easy_perform(curl);
-
-    /* close the header file */
-    fclose(fp);
+    fprintf ( stdout , "request status: %i\n", res ) ;
 
     /* cleanup curl stuff */
     curl_easy_cleanup(curl);
     curl_global_cleanup();
     return (int)res;
 }
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]){
 
     setlocale(LC_ALL, ".UTF-8");
 
     int res = 0;
 
-    const char *fName = "Ubike.json";
-    char *urlAddress = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json";
+    streamStruct * buffer = malloc ( sizeof ( streamStruct ) ) ;
+    buffer[0].data = NULL ; 
+    buffer[0].size = 0 ;
 
-    /*    res = donwloadFileFromURL(fName, urlAddress);
-        if (res == 1)
-        {
-            return EXIT_FAILURE;
+    char *urlAddress = "https://store.line.me/stickershop/product/36044/en" ;
+    res = curl_get ( buffer , urlAddress );
+    if (res == 1) { return EXIT_FAILURE; }
+
+
+    
+    htmlDocPtr doc = htmlReadMemory(
+        buffer->data,         // your HTML text
+        buffer->size,         // length of HTML
+        NULL,               // base URL (optional)
+        NULL,               // encoding (NULL = auto)
+        HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING
+    );
+
+    //puts( buffer->data ) ;
+    free( buffer->data ) ;
+    free( buffer ) ;
+
+    if (!doc) {
+        fprintf(stdout ,  "Parse fail!\n");
+        return  EXIT_FAILURE ;
+    }
+    
+    xmlXPathContextPtr ctx = xmlXPathNewContext(doc);
+    xmlXPathObjectPtr r = xmlXPathEvalExpression((xmlChar*)"//h1", ctx);
+
+    printf("found: %d h1\n", r && r->nodesetval ? r->nodesetval->nodeNr : 0);
+
+    if (r && r->nodesetval && r->nodesetval->nodeNr > 0) {
+        for ( unsigned int i = 0 ; i < r->nodesetval->nodeNr ; i++ ) {
+            xmlNodePtr n = r->nodesetval->nodeTab[i];
+            xmlChar *text = xmlNodeGetContent(n);
+            printf("TEXT: %s\n", text);
+            xmlFree(text);
         }
-    */
-    // open the file
+    }
+    xmlXPathFreeObject(r);  
+    xmlXPathFreeContext(ctx);
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+  
+
+
+    
    
     return EXIT_SUCCESS;
 }
